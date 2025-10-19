3f9bbf36fbc0bcee4ead521506a3ea3c235122ef
AshleyNyamandi <nyamandiashley@gmail.com>
2025-10-19 09:31:52 +0200

    fixed font bugs

--- src/components/Container.jsx
+++ src/components/Container.jsx
@@ -1,6 +1,6 @@
 export default function Container({ children }) {
     return (
-        <div className='px-2 sm:px-5 py-3 sm:p-14 max-w-3xl mx-auto'>
+        <div className='px-2 sm:px-5 py-3 sm:p-14 max-w-7xl mx-auto'>
             { children }
         </div>
     )
--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,4 +1,6 @@
+import { useState } from 'react'
 import Container from '../components/Container'
+
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { library } from '@fortawesome/fontawesome-svg-core'
@@ -11,8 +13,31 @@ import { fab } from '@fortawesome/free-brands-svg-icons'
 library.add(fas, far, fab)
 
 export default function Contact() {
+        const [result, setResult] = useState("")
+
+    const onSubmit = async (e) => {
+        e.preventDefault()
+        setResult("Sending...")
+
+        const formData = new FormData(e.target)
+        formData.append("access_key", "ac6c0fc1-6ab9-49d0-b336-3f574739398e")
+
+        const res = await fetch("https://api.web3forms.com/submit", {
+            method: "POST",
+            body: formData,
+        }).then(r => r.json())
+
+        if (res.success) {
+            setResult(res.message)
+            e.target.reset()
+        } else {
+            setResult('Submission failed. Try again later')
+            console.error('web3forms:', res)
+        }
+    }
+
     return (
-        <section id='contact' className='text-lg lg:text-2xl lg:font-light'>
+        <section id='contact' className='text-lg lg:text-xl lg:font-light'>
             <div className='w-full max-w-6xl mx-auto    '>
                 <div className='text-center'>
                     <h3 className='pb-4 text-3xl font-bold'>Contact</h3>
@@ -34,11 +59,11 @@ export default function Contact() {
                             <span> Email: nyamandiashley@gmail.com</span>
                         </div>
                     </div>
-                    <form className='grow-2 p-3 rounded-sm shadow-[0_0_20px_rgba(0,0,0,0.05)]'>
+                    <form onSubmit={onSubmit} className='grow-2 p-3 rounded-sm shadow-[0_0_20px_rgba(0,0,0,0.05)]'>
                         <div className='flex flex-col mb-4'>
-                            <label htmlFor='name'>Full Name<span aria-hidden="true">*</span></label>
+                            <label className='mb-2' htmlFor='name'>Full Name<span aria-hidden="true">*</span></label>
                             <input
-                                className='border-2 border-black focus:border-4 rounded-sm px-2 py-1'
+                                className='border-2 border-black focus:border-4 rounded-sm px-2 py-1 text-base'
                                 type='text'
                                 id='name'
                                 placeholder='Tatenda Mhofu'
@@ -46,8 +71,8 @@ export default function Contact() {
                             />
                         </div>
                         <div className='flex flex-col mb-4'>
-                            <label htmlFor='email'>Email<span aria-hidden="true">*</span></label>
-                            <input className='border-2 border-black focus:border-4 rounded-sm px-2 py-1'
+                            <label className='mb-2' htmlFor='email'>Email<span aria-hidden="true">*</span></label>
+                            <input className='border-2 border-black focus:border-4 rounded-sm px-2 py-1 text-base'
                                 type='email'
                                 id='email'
                                 placeholder='johndoe@company.com'
@@ -55,16 +80,17 @@ export default function Contact() {
                             />
                         </div>
                         <div className='flex flex-col mb-4'>
-                            <label htmlFor='message'>Message<span aria-hidden="true">*</span></label>
+                            <label className='mb-2' htmlFor='message'>Message<span aria-hidden="true">*</span></label>
                             <textarea 
-                                className='border-2 border-black focus:border-4 rounded-sm px-2 py-1 resize-none w-full h-24'
+                                className='border-2 border-black focus:border-4 rounded-sm px-2 py-1 resize-none w-full h-24 text-base'
                                 id='message' 
                                 placeholder='What is on your mind?'
                                 required>
                             </textarea>
                         </div>
 
-                        <button type='submit' className='bg-slate-950 text-slate-50 py-2 px-4 max-w-fit hover:bg-slate-300 hover:text-slate-950 active:bg-slate-300  active:text-slate-950 focus:bg-slate-300 focus:text-slate-950 rounded-sm mt-3 sm:text-2xl'>Send</button>
+                        <button type='submit' className='bg-slate-950 text-slate-50 py-2 px-4 max-w-fit hover:bg-slate-300 hover:text-slate-950 active:bg-slate-300  active:text-slate-950 focus:bg-slate-300 focus:text-slate-950 rounded-sm mt-3 lg:text-2xl mr-6'>Send</button>
+                        <span>{result}</span>
                     </form>
                 </div>
             </div>
