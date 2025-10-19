ceb75f2342968cf68b936e4f36dbfa7027f1b751
AshleyNyamandi <nyamandiashley@gmail.com>
2025-10-19 14:12:10 +0200

    band member image width fix

--- src/components/BandMember.jsx
+++ src/components/BandMember.jsx
@@ -2,8 +2,8 @@ export default function BandMember({ name, img, alt }) {
     return (
         <div className='shrink-0'>
             <span className='block pb-3 text-center text-red-950 font-medium sm:text-lg'aria-hidden='true'>{name}</span> 
-            <div className='w-36 h-62 overflow-hidden opacity-80 outline-0 rounded-2xl'>
-                <img className='w-full h-full object-cover object-center' src={img} alt={alt} />
+            <div className='w-full max-w-3xs overflow-hidden opacity-80 outline-0 rounded-2xl'>
+                <img className='w-full aspect-9/16  object-cover object-center' src={img} alt={alt} />
             </div>
         </div>
     )
